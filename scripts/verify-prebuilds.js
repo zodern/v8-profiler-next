f161d0d2e4fabc37719c7acb57b4175ee21db56a
zodern <zodern@users.noreply.github.com>
2026-04-30 19:10:17 -0500

    Add prepublish script to verify prebuilds

---
+++ scripts/verify-prebuilds.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const cp = require('child_process');
+const fs = require('fs');
+const path = require('path');
+
+const root = path.join(__dirname, '..');
+const prebuildsDir = path.join(root, 'prebuilds');
+
+if (!fs.existsSync(prebuildsDir)) {
+  console.error('prebuilds/ does not exist');
+  process.exit(1);
+}
+
+// If the binaries were downloaded from the github workflow, they might be quarantined
+if (process.platform === 'darwin') {
+  try {
+    cp.execFileSync('xattr', ['-dr', 'com.apple.quarantine', prebuildsDir], { stdio: 'ignore' });
+  } catch (e) {
+    // xattr errors when the attribute isn't present
+  }
+}
+
+const dirents = fs.readdirSync(prebuildsDir).filter(n => !n.startsWith('.'));
+if (dirents.length !== 5) {
+  throw new Error(`Missing platforms: ${dirents.length} !== 5`);
+}
+
+let buildsCount = 0;
+for(const platform of dirents) {
+  const binaries = fs.readdirSync(path.join(prebuildsDir, platform));
+  buildsCount += binaries.length;
+
+  for(const binaryName of binaries) {
+    let stats = fs.statSync(path.join(prebuildsDir, platform, binaryName));
+
+    if (stats.size < 1024 * 20) {
+      throw new Error(`Binary too small - ${platform}/${binaryName}`);
+    }
+  }
+}
+
+if (buildsCount !== 13) {
+  throw new Error(`Wrong number of builds: ${buildsCount}`);
+}
+
+require('./verify-prebuild');
+
+console.log('All checks passed');
